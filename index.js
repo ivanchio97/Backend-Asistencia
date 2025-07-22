2ca6642817969d9a15ada1cf6473f7506b07a7fa
ivanchio97 <ing.ivanchio@gmail.comgit config --global user.email ing.ivanchio@gmail.comgit config --global user.email ing.ivanchio@gmail.com>
2025-07-21 18:09:32 -0600

    fixed URL CORS

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ['https://asistencia-icastillo.netlify.app/'],
+    origin: ['https://asistencia-icastillo.netlify.app'],
     credentials: true
 }));
 
